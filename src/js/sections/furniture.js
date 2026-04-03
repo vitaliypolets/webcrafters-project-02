e4ec5730f6b883883c21540a36d384c18c1141c0
Vitaliy <vspolets@gmail.com>
2026-04-03 09:49:18 +0300

    uodate css

--- src/js/sections/furniture.js
+++ src/js/sections/furniture.js
@@ -6,55 +6,55 @@ import { renderFurnitureCards } from '../render/render-furniture.js';
 const mockCategories = [
   {
     name: 'Всі товари',
-    image: '/categories/categories_all.jpg',
+    image: './categories/categories_all.jpg',
   },
   {
     name: 'Мякі меблі',
-    image: '/categories/categories1.jpg',
+    image: './categories/categories1.jpg',
   },
   {
     name: 'Шафи та системи зберігання',
-    image: '/categories/categories2.jpg',
+    image: './categories/categories2.jpg',
   },
   {
     name: 'Ліжка та матраци',
-    image: '/categories/categories3.jpg',
+    image: './categories/categories3.jpg',
   },
   {
     name: 'Столи',
-    image: '/categories/categories4.jpg',
+    image: './categories/categories4.jpg',
   },
    {
     name: 'Стільці та табурети',
-    image: '/categories/categories5.jpg',
+    image: './categories/categories5.jpg',
   },
     {
     name: 'Кухні',
-    image: '/categories/categories6.jpg',
+    image: './categories/categories6.jpg',
   },
      {
     name: 'Меблі для дитячої',
-    image: '/categories/categories7.jpg',
+    image: './categories/categories7.jpg',
   },
       {
     name: 'Меблі для офісу',
-    image: '/categories/categories8.jpg',
+    image: './categories/categories8.jpg',
   },
     {
     name: 'Меблі для передпокою',
-    image: '/categories/categories9.jpg',
+    image: './categories/categories9.jpg',
   },   
      {
     name: 'Меблі для ванної кімнати',
-    image: '/categories/categories10.jpg',
+    image: './categories/categories10.jpg',
   },
      {
     name: 'Садові та вуличні меблі',
-    image: '/categories/categories11.jpg',
+    image: './categories/categories11.jpg',
   },
       {
     name: 'Декор та аксесуари',
-    image: '/categories/categories12.jpg',
+    image: './categories/categories12.jpg',
   },
 ];
 
@@ -64,56 +64,56 @@ const mockFurniture = [
     name: 'Сучасний диван',
     price: 12500,
     colors: ['Бежевий'],
-    images: ['/images/furniture1.jpg'],
+    images: ['./images/furniture1.jpg'],
   },
   {
     _id: '2',
     name: 'Деревʼяний стілець',
     price: 3200,
     colors: ['Горіх'],
-    images: ['/images/furniture2.jpg'],
+    images: ['./images/furniture2.jpg'],
   },
   {
     _id: '3',
     name: 'Журнальний столик',
     price: 5400,
     colors: ['Дуб'],
-    images: ['/images/furniture3.jpg'],
+    images: ['./images/furniture3.jpg'],
   },
   {
     _id: '4',
     name: 'М’яке ліжко',
     price: 18900,
     colors: ['Сірий'],
-    images: ['/images/furniture4.jpg'],
+    images: ['./images/furniture4.jpg'],
   },
   {
     _id: '5',
     name: 'М’яке ліжко',
     price: 18900,
     colors: ['Сірий'],
-    images: ['/images/furniture5.jpg'],
+    images: ['./images/furniture5.jpg'],
   },
   {
     _id: '6',
     name: 'М’яке ліжко',
     price: 18900,
     colors: ['Сірий'],
-    images: ['/images/furniture6.jpg'],
+    images: ['./images/furniture6.jpg'],
   },
   {
     _id: '7',
     name: 'М’яке ліжко',
     price: 18900,
     colors: ['Сірий'],
-    images: ['/images/furniture7.jpg'],
+    images: ['./images/furniture7.jpg'],
   },
   {
     _id: '8',
     name: 'М’яке ліжко',
     price: 18900,
     colors: ['Сірий'],
-    images: ['/images/furniture8.jpg'],
+    images: ['./images/furniture8.jpg'],
   },
 ];
 
